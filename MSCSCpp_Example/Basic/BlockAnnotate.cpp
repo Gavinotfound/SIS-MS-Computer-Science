51dd2b1addf8f51cdf09bc7bb336d27e93375dde
Gavinotfound <101452310+Gavinotfound@users.noreply.github.com>
2023-11-19 21:53:08 +0800

    BasicPt.1
    C++
    Calculations and annotate.

---
+++ MSCSCpp_Example/Basic/BlockAnnotate.cpp
@@ -0,0 +1,6 @@
+/*
+A block annotate in C++ is like this one.
+it starts with"/*" and ends with the signal below.
+*/
+
+
---
+++ MSCSCpp_Example/Basic/Calculations.cpp
@@ -0,0 +1,22 @@
+// Calculating
+/*
+In C++ and many other coding lang, the math calculation signs follow the rules below:
+Add: "+"
+Minus: "-"
+Multiply: "*"
+Divide: "/"
+Mod: "%"
+Self Add/Minus 1: "++/--"
+*/
+// See examples below
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i = ((1+2)/3*839/3)%1;
+	// this is 0
+	cout<< i <<endl;
+	return 0;
+}
+
